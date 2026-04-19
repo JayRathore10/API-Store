201c2e80c8dd2b2932be0541f70c0649337c939a
Jay <jayrathore88155@gmail.com>
2026-04-19 16:48:36 +0530

    Add test should return 201 when user is created successfully

--- server/src/tests/auth.test.js
+++ server/src/tests/auth.test.js
@@ -2,10 +2,14 @@ import request from "supertest";
 import User from "../models/user.model.js";
 import { expect, jest } from "@jest/globals";
 
-
 const mockUser = {
   findOne: jest.fn(),
   create: jest.fn(),
+};
+
+const mockBcrypt = {
+  genSalt: jest.fn(),
+  hash: jest.fn()
 };
 
 jest.unstable_mockModule("../models/user.model.js", () => ({
@@ -28,22 +32,49 @@ describe("POST /api/v1/auth/register", () => {
     expect(res.status).toBe(400);
   });
 
-  test("should return 400 when user already exists" , async()=>{
+  test("should return 400 when user already exists", async () => {
     mockUser.findOne.mockResolvedValue({
-      _id : "123"  , 
-      name : "test" , 
-      email : "test@gmail.com"
+      _id: "123",
+      name: "test",
+      email: "test@gmail.com"
     });
 
     const res = await request(app).
       post("/api/v1/auth/register").
       send({
-        name : "test" , 
-        email : "test@gmail.com" , 
+        name: "test",
+        email: "test@gmail.com",
         password: "test123"
       });
 
-      expect(res.status).toBe(400);
+    expect(res.status).toBe(400);
+  });
+
+  test("should return 201 when user is created successfully", async () => {
+    mockUser.findOne.mockResolvedValue(null);
+
+    mockBcrypt.genSalt.mockResolvedValue("*");
+    mockBcrypt.hash.mockResolvedValue("123**");
+
+    mockUser.create.mockResolvedValue({
+      name: "test",
+      email: "test@gmail.com",
+      password: "123**" , 
+      generateToken: jest.fn().mockReturnValue("fake-token"),
+    });
+
+
+
+    const res = await request(app).
+      post("/api/v1/auth/register").
+      send({
+        name: "test",
+        email: "test@gmail.com",
+        password: "test"
+      });
+
+    expect(res.status).toBe(201);
 
   })
+
 })
